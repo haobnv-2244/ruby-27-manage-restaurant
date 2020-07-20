143499f79c69b3626848e91482ac262daa64539b
Nguyen Van Hao B <nguyen.van.haob@sun-asterisk.com>
2020-07-20 11:29:53 +0700

    fix bug validate

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -3,6 +3,11 @@ class Dish < ApplicationRecord
     images_attributes: [:id, :image, :image_cache, :_destroy]].freeze
   belongs_to :dish_type
   has_many :images, dependent: :destroy
+
+  validates :name,  presence: true
+  validates :price, numericality: {greater_than_or_equal_to: Settings.dishes.price.min_value},
+            length: {maximum: Settings.dishes.price.max_length}
+
   accepts_nested_attributes_for :images, allow_destroy: true, reject_if: :all_blank
 
   enum status: {out_of_stock: 0, in_stock: 1}
--- app/models/dish_type.rb
+++ app/models/dish_type.rb
@@ -2,5 +2,7 @@ class DishType < ApplicationRecord
   DISH_TYPES_PARAMS = %i(name description).freeze
   has_many :dishes, dependent: :destroy
 
+  validates :name, presence: true
+
   scope :dish_types, ->{order "name ASC"}
 end
